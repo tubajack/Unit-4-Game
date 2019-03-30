99aa50d7cac03d49939254ea376bc0f07e9bc8f0
Jackson Henry <jackson.henry13@yahoo.com>
2019-03-29 23:30:29 -0500

    Alerts are annoying in JavaScript. I removed the alerts that are in my game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,33 +6,21 @@ var score = 0;
 $(".baylor-logo").on("click", function(){
     score = score + 3;
     scoreCounter.textContent = "Your score is: " + score;
-
-    //Alert the user they clicked the Baylor logo
-    alert("You clicked the Baylor logo. Sic' Em");
 });
 
 $(".notredame-logo").on("click", function(){
     score = score + 2;
     scoreCounter.textContent = "Your score is: " + score;
-
-    //Alert the user they clicked the Baylor logo
-    alert("You clicked the Notre Dame logo. Go Irish");
 });
 
 $(".oregon-logo").on("click", function(){
     score = score + 5;
     scoreCounter.textContent = "Your score is: " + score;
-
-    //Alert the user they clicked the Baylor logo
-    alert("You clicked the Oregon logo. Go Ducks");
 });
 
 $(".uconn-logo").on("click", function(){
     score = score + 10;
     scoreCounter.textContent = "Your score is: " + score;
-
-    //Alert the user they clicked the Baylor logo
-    alert("You clicked the UConn logo. Go Huskies");
 });
 
 //Create a variable for holding a reference for the score counter

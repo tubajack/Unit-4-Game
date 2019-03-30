ef6495a43829ff903d3f8cc986996046fb1ee08a
Jackson Henry <jackson.henry13@yahoo.com>
2019-03-29 23:38:55 -0500

    Added a target score. Am getting object HTMLParagraphElement as my result. Have a little work to do to fix that.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
 //Declare a variable for the score
 var score = 0;
 
+//Declare a target score
+var targetScore = 100; 
+
 
 //click an event which responds to the clicking of the logos
 $(".baylor-logo").on("click", function(){
@@ -25,6 +28,8 @@ $(".uconn-logo").on("click", function(){
 
 //Create a variable for holding a reference for the score counter
 var scoreCounter = document.getElementById("score-counter")
+var targetScore = document.getElementById("target-score");
 
 //Have that score be displayed to the screen
 scoreCounter.textContent = "Your score is: " + score;
+targetScore.textContent = "Target Score: " + targetScore;
